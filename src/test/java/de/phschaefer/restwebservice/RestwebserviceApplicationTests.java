0f228b50d6abb04697ce2aca40527f6c05f04d34
Philipp <ph.schaefer@protonmail.com>
2020-01-30 22:45:00 +0100

    Initial commit

---
+++ src/test/java/de/phschaefer/restwebservice/RestwebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.phschaefer.restwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestwebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
